8356e577fceeb026afeb3a6913af51f15248934d
priyamohan08 <109510877+priyamohan08@users.noreply.github.com>
2022-07-28 13:38:51 +0530

    Add files via upload

---
+++ ways to assign variables.py
@@ -0,0 +1,37 @@
+"""First way"""
+# a=6
+# b=5
+# c=6
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+
+"""Second way"""
+# a=4;b=5;c=6
+# print(a)
+# print(b)
+# print(c)
+
+"""Third way"""
+# a=6;b=7;c=8
+# print(a) ;print(b) ;print(c)
+
+"""Fourth Way"""
+# a,b,c,d=7,8,9,4
+# print(a) ;print(b) ;print(c) ;print(d)
+
+"""Fifth way"""
+# a=3;b=5;c=7
+# print(a,b,c)
+# print(a,a,a,a,a)
+# print(b,c,a)
+
+"""Sixth Way"""
+# a=b=e=i=o=u=8
+# print(b);print(u);print(o);print(b)
+
+"""seventh Way"""
+# a=d=e=5;b=7;c=3
+# print(a,e,c)
+# print(d,b,a,c)
